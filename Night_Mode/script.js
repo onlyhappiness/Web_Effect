e3267cb85690b00748d95af54aea59d06805e91c
onlyhappiness <onlyhappiness96@gmail.com>
2021-05-12 22:03:41 +0900

    Night_Mode

---
+++ Night_Mode/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+
+
+toggle.onclick = function() {
+  toggle.classList.toggle('active');
+  body.classList.toggle('active');
+}
